c3fe85386d7231983905cb0140fedaffcdec2814
Keira Jarvis <82812348+KeiraJCoder@users.noreply.github.com>
2021-12-02 14:45:36 +0000

    Create ghostbusters.py

---
+++ ghostbusters.py
@@ -0,0 +1,19 @@
+def film_check():
+    print("Who you going to call??")
+    
+
+    fave_films = ["Stargate", 
+        "Star Trek VI", 
+        "Ghostbusters",
+        "Godzila: king of the monster",
+    ]
+    
+    def film_search():
+
+        if (fave_films [2]) == "Ghostbusters":
+            print("yay Ghostbusters")
+        else:
+            print("Boo not Ghostbusters")
+    
+    film_search()
+film_check()
